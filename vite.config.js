d8069ba057176bc86ab6c2e4e1b9d677f73e2684
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-04-21 21:25:36 -0600

    live release config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const isGitHubPages = process.env.BUILD_TARGET === "gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGitHubPages ? "/Sunshine_University/" : "./",
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+});
